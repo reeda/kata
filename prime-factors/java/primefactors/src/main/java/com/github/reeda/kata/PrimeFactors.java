8f522dc47020a922eb5f0dc1dff7c19625a5ba7e
Andrew Reed <areed@Andrews-MacBook-Pro.local>
2016-03-12 10:32:39 -0500

    java prime factors

--- prime-factors/java/primefactors/src/main/java/com/github/reeda/kata/PrimeFactors.java
+++ prime-factors/java/primefactors/src/main/java/com/github/reeda/kata/PrimeFactors.java
@@ -2,11 +2,15 @@ package com.github.reeda.kata;
 
 import com.google.common.collect.Lists;
 import java.util.List;
-import java.lang.Integer;
 
-public class PrimeFactors 
-{
+public class PrimeFactors {
     public static List<Integer> generate(int n) {
-	return Lists.newArrayList();
+        List<Integer> factors = Lists.newArrayList();
+        for (int candidate = 2; n > 1; candidate++) {
+            for (; n % candidate == 0; n /= candidate) {
+                factors.add(candidate);
+            }
+        }
+	    return factors;
     }
 }
--- prime-factors/java/primefactors/src/test/java/com/github/reeda/kata/PrimeFactorsTest.java
+++ prime-factors/java/primefactors/src/test/java/com/github/reeda/kata/PrimeFactorsTest.java
@@ -1,12 +1,44 @@
 package com.github.reeda.kata;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
-import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
 
 public class PrimeFactorsTest {
 
     @Test
     public void testOne() {
-	Assert.assertEquals(Lists.newArrayList(), PrimeFactors.generate(1));
+	    assertEquals(Lists.newArrayList(), PrimeFactors.generate(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(Lists.newArrayList(2), PrimeFactors.generate(2));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(Lists.newArrayList(3), PrimeFactors.generate(3));
+    }
+
+    @Test
+    public void testFour() {
+        assertEquals(Lists.newArrayList(2, 2), PrimeFactors.generate(4));
+    }
+
+    @Test
+    public void testSix() {
+        assertEquals(Lists.newArrayList(2, 3), PrimeFactors.generate(6));
+    }
+
+    @Test
+    public void testEight() {
+        assertEquals(Lists.newArrayList(2, 2, 2), PrimeFactors.generate(8));
+    }
+
+    @Test
+    public void testNine() {
+        assertEquals(Lists.newArrayList(3, 3), PrimeFactors.generate(9));
     }
 }
